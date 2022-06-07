7839b9b675afd77539dd1eac27fdc45b5befd91c
JoseMiranda86 <josemiranda_86@hotmail.com>
2022-06-07 21:53:44 +0900

    Refactor

--- Web_Data.py
+++ Web_Data.py
@@ -54,34 +54,3 @@ for word in dictio:
             print(dictio[0])
             webbrowser.open('https://google.com/finance/options/+'diction[0]'')
             exit()
-
-print('\n', 'PRICES','\n')
-count = 0
-for Quote in list:
-    if Quote.startswith("''") or Option.startswith(" ''") or Option.startswith(" "):
-        count = count + 1
-        Price = Quote.rstrip()
-        Expiration = Price.strip()
-        pos = Expiration.find('\n')
-        pos2 = Expiration.find('<ref>')
-        if pos == -1 and pos2 == -1:
-            print(count,'-',Expiration)
-        elif pos != -1:
-            print(count,'-',Expiration[0:pos])
-        else:
-            print(count,'-',Expiration[0:pos2])
-
-print('\n')  
-
-while True:
-    address = input('\n'+'Enter location: ')
-    typeofbusiness = input('\n'+'Provide the type of business to search: ')
-    if len(address) < 1: break
-
-    parms = dict()
-    parms['address'] = address
-    parms['key'] = api_key
-    url = serviceurl + urllib.parse.urlencode(parms)
-
-    uh = urllib.request.urlopen(url, context=ctx)
-    data = uh.read().decode()
