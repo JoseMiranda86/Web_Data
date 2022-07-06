81de52ed0ee03a1de8cfe3a54cbcfdd1b3094266
JoseMiranda86 <josemiranda_86@hotmail.com>
2022-07-06 22:30:07 +0900

    Back

--- Web_Data.py
+++ Web_Data.py
@@ -1,3 +1,4 @@
+from optparse import Option
 import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
@@ -17,15 +18,31 @@ Jason = json.loads(Data)
 try:
         SectionIndex = Jason['parse']['sections']
         for element in SectionIndex:
-            for a, b in element.items():
-                if b == 'Options':
+            for x, y in element.items():
+                if y == 'Options':
                     index = element['index']
-                    Signal = -2
 except:
         index = 0
 
 if index == 0:
     ServiceURL = 'https://google.com/finance/options'
+    uh = urllib.request.urlopen(ServiceURL, context=ctx)
+
+    Data = uh.read().decode()
+    Jason = json.loads(Data)
+
+    try:
+        SectionIndex = Jason['parse']['sections']
+        for element in SectionIndex:
+            for a, b in element.items():
+                if b == 'Price':
+                    index = element['index']
+                    Signal = -1
+    except:
+        index = 0   
+
+if index == 0:
+    ServiceURL = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page='+NameURL+'&prop=sections&disabletoc=1'
     uh = urllib.request.urlopen(ServiceURL, context=ctx)
 
     Data1 = uh.read().decode()
@@ -35,52 +52,15 @@ if index == 0:
         SectionIndex = Jason1['parse']['sections']
         for element in SectionIndex:
             for a, b in element.items():
-                if b == 'Price':
+                if b == 'Discography':
                     index = element['index']
                     Signal = -1
     except:
         index = 0   
 
-uh2 = urllib.request.urlopen(serviceurl2, context=ctx)
-data2 = uh2.read().decode()
-Jason2 = json.loads(data2)
-
-print('\n')
-
-SectionIndex2 = Jason2['parse']['wikitext']
-
-x = SectionIndex2['*']
-list = x.split("*")
-list2 = list[0].split('\n')
-
-for word in list2:
-    if word.startswith('{| '):
-        if Signal == -1:
-            print('Options prices')
-            webbrowser.open('https://google.com/finance/options')
-            exit()
-
-print('\n', 'Options','\n')
-count = 0
-for Set in list:
-    if Set.startswith("''") or Set.startswith(" ''") or Set.startswith(" "):
-        count = count + 1
-        Set2 = Set.rstrip()
-        Set3 = Set2.strip()
-        pos = Set3.find('\n')
-        pos2 = Set3.find('<ref>')
-        if pos == -1 and pos2 == -1:
-            print(count,'-',Set3)
-        elif pos != -1:
-            print(count,'-',Set3[0:pos])
-        else:
-            print(count,'-',Set3[0:pos2])
-
-print('\n')      
-
 if index == 0:
-    serviceurl1 = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page='+NameURL+'_(singer)'+'&prop=sections&disabletoc=1'
-    uh = urllib.request.urlopen(serviceurl1, context=ctx)
+    serviceurl1 = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page='+NameURL+'_(band)'+'&prop=sections&disabletoc=1'
+    uh = urllib.request.urlopen(ServiceURL, context=ctx)
 
     Data1 = uh.read().decode()
     Jason1 = json.loads(Data1)
@@ -91,35 +71,6 @@ if index == 0:
             for a, b in element.items():
                 if b == 'Discography':
                     index = element['index']
-                    Signal = 2
+                    Signal = 1
     except:
-        index = 0      
-
-if index == 0:
-    serviceurl1 = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page='+NameURL+'_(musician)'+'&prop=sections&disabletoc=1'
-    uh = urllib.request.urlopen(serviceurl1, context=ctx)
-
-    Data1 = uh.read().decode()
-    Jason1 = json.loads(Data1)
-
-    try:
-        SectionIndex = Jason1['parse']['sections']
-        for element in SectionIndex:
-            for a, b in element.items():
-                if b == 'Discography':
-                    index = element['index']
-                    Signal = 3
-    except:
-        index = 0
-
-if index == 0:
-    NAME1 = NameURL.upper()
-    serviceurl1 = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page='+NAME1+'&prop=sections&disabletoc=1'
-    uh = urllib.request.urlopen(serviceurl1, context=ctx)
-
-    Data1 = uh.read().decode()
-    Jason1 = json.loads(Data1)
-    for x,y in Jason1.items():
-        if x == 'error':
-            print('\nName could not been found')
-            exit()
+        index = 0        
