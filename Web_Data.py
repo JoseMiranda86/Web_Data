74d8f171da019c269119dc9434eddb9503c180d9
JoseMiranda86 <josemiranda_86@hotmail.com>
2022-07-26 21:54:23 +0900

    Testing collect data

--- Web_Data.py
+++ Web_Data.py
@@ -41,3 +41,30 @@ if index == 0:
     except:
         index = 0   
         
+while True:
+    address = input('\n'+'Enter location: ')
+    typeofbusiness = input('\n'+'Provide the type of business to search: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+
+        print('Location can not be found. Try again')
+        continue
+
+    lat = str(js['results'][0]['geometry']['location']['lat'])
+    lng = str(js['results'][0]['geometry']['location']['lng'])
+    webbrowser.open('https://www.google.com/maps/search/'+typeofbusiness+'/@'+lat+','+lng+',15z/data=!3m1!4b1','\n')
+    exit()
